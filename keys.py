9b4e7b7b1fb773542bce462fad41c8c5befb5a9f
alexjacobs08 <jacobsad@g.cofc.edu>
2021-11-04 16:19:54 -0400

    Delete keys.py

--- keys.py
+++
@@ -1,6 +0,0 @@
-consumer_key='A2Tb37bLCpXfubDelIKKyD1wk'
-consumer_secret='T3eJEP2eNtth88jJh5i7I6zRRc7lomDk2qrp62BVxElbCkkSCR'
-access_token='4889433513-UCig0Sb9SXf3Uzaw0Sfg2UkwQSwsZWAPMtWHV1J'
-access_token_secret='PqS6PUBiRZbOnVuv3UiTjDCA1NY0zOGrGUVIPRxyJMaug'
-
-#tokens and keys at https://apps.twitter.com/app/9508708/keys
